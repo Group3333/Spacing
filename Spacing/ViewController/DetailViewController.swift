d9de360f6f2a9544d50c8a0d420ed2b7cd44e07e
hyelim9300 <hye930011@naver.com>
2024-04-23 19:59:39 +0900

    Refactor : 불필요한 코드 및 주석 삭제

--- Spacing/ViewController/DetailViewController.swift
+++ Spacing/ViewController/DetailViewController.swift
@@ -10,9 +10,6 @@ import UIKit
 class DetailViewController: UIViewController, TimeSelectionDelegate {
     
     // MARK: - Properties
-    var price = 0
-    var index = -1
-    
     var place: Place?
     
     // MARK: - IBOutlets
@@ -28,22 +25,15 @@ class DetailViewController: UIViewController, TimeSelectionDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.placeName.text = place?.title
-        self.placeCateg.text = place?.categories.first?.rawValue ?? "No Category"
-        self.placeDesc.text = place?.description
-        self.placePrice.text = "\(place?.price ?? 0)원"
+        configureUI()
     }
     
     // MARK: - IBActions
     
     @IBAction func timeSelectBtn(_ sender: Any) {
         guard let timeSelectionVC = UIStoryboard(name: "TimeSelectionSB", bundle: nil).instantiateViewController(withIdentifier: "TimeSelectionVC") as? TimeSelectionVC else {
-            // 스토리보드에서 TimeSelectionVC를 가져오지 못한 경우
-            // 오류 처리 또는 경고 메시지를 표시할 수 있습니다.
             return
         }
-        
         timeSelectionVC.modalPresentationStyle = .custom
         timeSelectionVC.transitioningDelegate = self
         timeSelectionVC.delegate = self
@@ -54,6 +44,16 @@ class DetailViewController: UIViewController, TimeSelectionDelegate {
         bookingAlert()
     }
     
+    // MARK: - Methods
+    
+    private func configureUI() {
+            placeName.text = place?.title ?? "A"
+            placeCateg.text = place?.categories.first?.rawValue ?? "No Category"
+            placeDesc.text = place?.description ?? "B"
+            placePrice.text = "\(place?.price ?? 0)원"
+            [placeName, placeCateg, placeDesc, placePrice].forEach { $0?.sizeToFit() }
+        }
+    
     func timeSelected(_ hours: Int) {
         print("대여 시간: \(hours)")
         let totalPrice = (place?.price ?? 0) * hours
@@ -62,14 +62,10 @@ class DetailViewController: UIViewController, TimeSelectionDelegate {
     
     func bookingAlert() {
         let bookingAlert = UIAlertController(title: nil, message: "예약이 완료되었습니다.", preferredStyle: .alert)
-        
-        // 메인화면으로 전환
         let ok = UIAlertAction(title: "확인", style: .default) {_ in 
-            self.dismiss(animated: true) // 뷰 컨트롤러 닫기
+            self.dismiss(animated: true)
         }
-        
         bookingAlert.addAction(ok)
-        
         present(bookingAlert, animated: true, completion: nil)
     }
 }
